9f8c2987a22aebbf09dcef4fa28828510c21ec51
unknown <yjs12180825@gmail.com>
2024-01-15 16:06:03 +0900

    :sparkles: Write page 추가 :sparkles:

--- lib/screens/write/write_view.dart
+++ lib/screens/write/write_view.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:ogam_diary/args/diaryArgument.dart';
 import 'package:ogam_diary/widgets/bottomSheet_widget.dart';
+import 'package:ogam_diary/widgets/button_widget.dart';
 
 class WritePage extends StatelessWidget {
   final formKey = GlobalKey<FormState>();
+
   WritePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController situationArea = TextEditingController();
+    TextEditingController emotionArea = TextEditingController();
     final args = ModalRoute.of(context)!.settings.arguments as DiaryArgument;
 
     return Scaffold(
@@ -34,10 +38,64 @@ class WritePage extends StatelessWidget {
       body: SafeArea(
         child: Form(
             key: formKey,
-            child: const Padding(
-              padding: EdgeInsets.all(6),
+            child: Padding(
+              padding: const EdgeInsets.all(6),
               child: Column(
-                children: [],
+                children: [
+                  Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(10),
+                      child: Column(children: [
+                        const Text(
+                          "기록할 사건을 입력해주세요",
+                          style: TextStyle(fontFamily: "Gugi-Regular"),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        TextField(
+                          style: const TextStyle(
+                              fontSize: 12, fontFamily: "Gugi-Regular"),
+                          controller: situationArea,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 5,
+                          decoration: const InputDecoration(
+                              hintText: "상황을 입력해주세요",
+                              hintStyle: TextStyle(fontFamily: "Gugi-Regular"),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1, color: Colors.black12))),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          "당시 감정은 어땠나요?",
+                          style: TextStyle(fontFamily: "Gugi-Regular"),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        TextField(
+                          controller: emotionArea,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 4,
+                          style: const TextStyle(
+                              fontSize: 12, fontFamily: "Gugi-Regular"),
+                          decoration: const InputDecoration(
+                              hintText: "생각을 입력해주세요",
+                              hintStyle: TextStyle(fontFamily: "Gugi-Regular"),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1, color: Colors.black12))),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        ButtonWidget().renderAuthButton(
+                            onTapHandler: () {}, label: "다음 페이지로")
+                      ]))
+                ],
               ),
             )),
       ),
